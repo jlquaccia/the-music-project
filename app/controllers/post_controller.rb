4392c0dbd827b4b32ef242bbea2828c4035f370e
Jason Quaccia <jlquaccia@gmail.com>
2015-10-16 10:47:20 -0700

    edited post controller and application.html.erb

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,9 +1,5 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.all
     @user = current_user
-  end
-
-  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:show]
   get 'newsfeed' => 'post#index'
 
   devise_for :users
