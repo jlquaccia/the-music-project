b009f3ab5f75374bb41f2aec19d1f6436223ab6a
Jason Quaccia <jlquaccia@gmail.com>
2015-10-19 12:23:27 -0700

    added functionality to search for artists via the spotify api

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
   def index
+    response = HTTParty.get("https://api.spotify.com/v1/search?q=#{CGI.escape params[:query]}&type=artist")
+    @hash_version = JSON.parse(response.body)
   end
 end
