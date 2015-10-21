a49820f7f274f9b433c4aaf36cfa9bc2989e7289
Jason Quaccia <jlquaccia@gmail.com>
2015-10-20 20:03:17 -0700

    made edits to artist index view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,6 @@
 class ArtistsController < ApplicationController
   def index
-    # params[:query] = 
+    # params[:query] = gets.chomp
 
     response = HTTParty.get("https://api.spotify.com/v1/search?q=#{CGI.escape params[:query]}&type=artist")
     @hash_version = JSON.parse(response.body)
