b0a333678304add934e7b06c9d69045cc175e1c4
Jason Quaccia <jlquaccia@gmail.com>
2015-10-26 12:34:01 -0700

    artist search really works now.. really

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,5 @@
 class ArtistsController < ApplicationController
   def index
-    # params[:query] = "http://localhost:3000/search?query=#{params[:name][:value]}"
     request = search_params[:query]
     unless request.nil?
       response = HTTParty.get("https://api.spotify.com/v1/search?q="+request+"&type=artist")
