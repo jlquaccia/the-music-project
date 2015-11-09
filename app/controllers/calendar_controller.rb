ee8dcf63de7e2666b5bb0312fbc404125b9e5ebc
Jason Quaccia <jlquaccia@gmail.com>
2015-11-08 18:14:16 -0800

    getting a users followed artists to display their upcoming concerts still a work in progress

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -8,10 +8,10 @@ class CalendarController < ApplicationController
     @user.follows.each do |follow|
       response = HTTParty.get("http://api.bandsintown.com/artists/#{URI.escape(follow.artist_name)}/events/search.json?api_version=2.0&app_id=el_proyecto_de_la_musica&location=use_geoip")
       @hash_version = JSON.parse(response.body)
-      
+
       @hash_version_array << @hash_version
     end
-    binding.pry
+
     @hash_version_array
   end
 
