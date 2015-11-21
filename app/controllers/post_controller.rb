a99a18abe277bfef0659f904db99ac7fccfe87e6
Jason Quaccia <jlquaccia@gmail.com>
2015-11-21 10:34:23 -0800

    can now successfully iterate through artist article posts from pitchfork via nokogiri webscraping

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,6 @@
 class PostController < ApplicationController
   def index
-    require 'rubygems'
-    require 'mechanize'
+    require 'nokogiri'
     require 'open-uri'
 
     @user = current_user
@@ -13,15 +12,8 @@ class PostController < ApplicationController
     end
     
     # binding.pry
-    @artist_name_array.each do |artist|
-      agent = Mechanize.new
-      page = agent.get("http://pitchfork.com/search/?query=#{artist}&filter=news")
-      @base_url = "http://pitchfork.com"
-      @href = page.search(".object-list h1 a")[0]["href"]
-      @title = page.search(".object-list h1 a")[0].text
-      @title_href = @base_url+@href
-      @description = page.search(".object-list p")[0].text
-      @date_and_time = page.search(".object-list h4")[0].text
-    end
+    url = 'http://pitchfork.com/search/more/?query=green%20day&filter=news'
+    @base_url = 'http://pitchfork.com'
+    @doc = Nokogiri::HTML(open(url))
   end
 end
