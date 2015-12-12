55a795dbe952d78e57a46881f26875c4b725ec69
Jason Quaccia <jlquaccia@gmail.com>
2015-12-12 15:28:28 -0800

    organized upcoming shows in chronological order

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -10,16 +10,26 @@ class CalendarController < ApplicationController
 
     @user.follows.each do |follow|
       # production
-      # response = HTTParty.get("http://api.bandsintown.com/artists/#{URI.escape(follow.artist_name)}/events/search.json?api_version=2.0&app_id=el_proyecto_de_la_musica&location=#{request.ip}")
+      response = HTTParty.get("http://api.bandsintown.com/artists/#{URI.escape(follow.artist_name)}/events/search.json?api_version=2.0&app_id=el_proyecto_de_la_musica&location=#{request.ip}")
 
       # development
-      response = HTTParty.get("http://api.bandsintown.com/artists/#{URI.escape(follow.artist_name)}/events/search.json?api_version=2.0&app_id=el_proyecto_de_la_musica&location=use_geoip")
+      # response = HTTParty.get("http://api.bandsintown.com/artists/#{URI.escape(follow.artist_name)}/events/search.json?api_version=2.0&app_id=el_proyecto_de_la_musica&location=use_geoip")
 
       @hash_version = JSON.parse(response.body)
       @hash_version_array << @hash_version if @hash_version != []
     end
 
-    @hash_version_array
+    @final_array = []
+
+    @hash_version_array.each do |subarray|
+      subarray.each do |third_array|
+        @final_array << third_array
+      end
+    end
+
+    @final_sort = @final_array.sort { |a,b| a['datetime'] <=> b['datetime'] }
+
+    # binding.pry
   end
 end
 
