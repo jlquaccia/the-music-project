72618aa11088f47a853614aad1439e8a64dc7152
Jason Quaccia <jlquaccia@gmail.com>
2015-12-16 12:33:07 -0800

    sorted artist search results by followers total

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,13 @@
 class ArtistsController < ApplicationController
   def index
     request = search_params[:query]
+    @hash_version_array = []
 
     if request
       response = HTTParty.get("https://api.spotify.com/v1/search?q=#{CGI.escape request}&type=artist")
       @hash_version = JSON.parse(response.body)
+
+      @hash_version_array << @hash_version["artists"]["items"].sort { |a, b| b['followers']['total'] <=> a['followers']['total'] }
     end
   end
 
